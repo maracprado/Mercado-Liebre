c80dca34a00617475d5d178d749aec61e6647470
maracprado <maracprado@gmail.com>
2021-10-08 20:52:13 -0300

    First backup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const path = require ('path');
+
+app = express();
+
+
+app.use(express.static('public'));
+
+app.get ('/', (req,res) => {
+    res.sendFile (path.join(__dirname,"/views/home.html"));
+})
+
+app.get ('/registro', (req,res) => {
+    res.sendFile (path.join(__dirname,"/views/registro.html"));
+})
+
+app.post ('/registro', (req,res) => {
+    res.redirect('/');
+})
+
+app.get ('/login', (req,res) => {
+    res.sendFile (path.join(__dirname,"/views/login.html"));
+})
+
+app.post ('/login', (req,res) => {
+    res.redirect('/');
+})
+
+
+app.listen (3030, ()=>{ console.log("Express está corriendo")})
